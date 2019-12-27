7c3d57b3c163759576a4809df9c95b34619abfcd
MrZahidAli <46484008+MrZahidAli@users.noreply.github.com>
2019-12-27 17:53:40 +0500

    formating done

--- syntaxOfR.R
+++ syntaxOfR.R
@@ -9,7 +9,7 @@ print(a)
 #printing multiple data types
 cat("value of a: ", a, "value of b: ", b)
 
-#chech data type of variable a
+#check data type of variable a
 
 print(class(a))
 
@@ -68,7 +68,7 @@ print(d >= v)
 print(d <= v)
 print(d != v)
 
-#logival operator
+#logical operator
 print(d & v)
 print(d | v)
 print(!v)
@@ -206,3 +206,39 @@ for (i in v) {
   arr[i] <- readline(prompt = "enter value: ")
 }
 
+#printing multiple strings
+
+a<-"zahid"
+b<-"ali"
+c<-"raina"
+print(paste(c,a,b,sep="-", collapse = ""))
+
+#Formating
+
+# Total number of digits displayed. Last digit rounded off.
+result <- format(23.123456789, digits = 9)
+print(result)
+
+# Display numbers in scientific notation.
+result <- format(c(6, 13.14521), scientific = TRUE)
+print(result)
+
+# The minimum number of digits to the right of the decimal point.
+result <- format(23.47, nsmall = 5)
+print(result)
+
+# Format treats everything as a string.
+result <- format(6)
+print(result)
+
+# Numbers are padded with blank in the beginning for width.
+result <- format(13.7, width = 6)
+print(result)
+
+# Left justify strings.
+result <- format("Hello", width = 8, justify = "1")
+print(result)
+
+# Justfy string with center.
+result <- format("Hello", width = 8, justify = "c")
+print(result)
